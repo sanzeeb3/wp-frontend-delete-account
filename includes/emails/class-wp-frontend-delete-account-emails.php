ca951fef23b2073393acc13652f2aaff23251fbd
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2020-04-20 11:12:36 +0545

    Build overview page

--- includes/emails/class-wp-frontend-delete-account-emails.php
+++ includes/emails/class-wp-frontend-delete-account-emails.php
@@ -24,6 +24,49 @@ class WPFDA_Emails {
 			</div>
 			<hr class="wp-header-end">
 			<hr/>
+		<tr valign="top">
+		<td class="wc_emails_wrapper" colspan="3">
+			<table style="width:75%" class="wc_emails widefat" cellspacing="0">
+				<thead>
+					<tr>
+						<?php
+						$columns = apply_filters(
+							'wp_frontend_delete_account_email_setting_columns',
+							array(
+								'status'     => '',
+								'name'       => __( 'Email', 'woocommerce' ),
+								'recipient'  => __( 'Recipient(s)', 'woocommerce' ),
+								'actions'    => __( 'Actions', 'woocommerce' ),
+							)
+						);
+						foreach ( $columns as $key => $column ) {
+							echo '<th class="wc-email-settings-table-' . esc_attr( $key ) . '">' . esc_html( $column ) . '</th>';
+						}
+						?>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="email-status">ON</td>
+							<td class="email-name"><?php esc_html_e( 'Admin Email', 'wp-frontend-delete-account' );?> </td>
+							<td class="email-receipent"><?php echo get_option( 'admin_email' ); ?> </td>
+							<td class="wc-email-settings-table-actions">
+								<a class="button" href=""><?php esc_html_e( 'Manage', 'wp-frontend-delete-account') ;?></a>
+							</td>
+						</tr>
+
+						<tr>
+							<td class="email-status">OFF</td>
+							<td class="email-name"><?php esc_html_e( 'User Email', 'wp-frontend-delete-account' );?> </td>
+							<td class="email-receipent"> <?php esc_html_e( 'User\'s Email Address', 'wp-frontend-delete-account' );?> </td>
+							<td class="wc-email-settings-table-actions">
+								<a class="button" href=""><?php esc_html_e( 'Manage', 'wp-frontend-delete-account') ;?></a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
 		<?php
 	}
 }
