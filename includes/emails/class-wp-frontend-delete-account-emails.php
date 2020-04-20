f5f4205b51707d9eb324c50c4591a65bf00e0497
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2020-04-20 10:20:46 +0545

    Heading and subheading add

--- includes/emails/class-wp-frontend-delete-account-emails.php
+++ includes/emails/class-wp-frontend-delete-account-emails.php
@@ -16,6 +16,14 @@ class WPFDA_Emails {
 	 * @return void.
 	 */
 	public static function overview() {
-		echo 'Yes, I am here';
+		?>
+			<h2 class="wp-heading-inline"><?php esc_html_e( 'Email Notifications', 'wp-frontend-delete-account' ); ?></h2>
+			<div id="email_notification_settings-description">
+				<p><?php esc_html_e( 'Email notifications sent from WP Frontend Delete Account are listed below. Click on an email to configure it.', 'wp-frontend-delete-account'); ?></p>
+				<p><?php esc_html_e( '** IMPORTANT: If you are having issue with email delivery, I recommend setting up SMTP in your site using SMTP plugins such as WP Mail SMTP.', 'wp-frontend-delete-account' ); ?></p>
+			</div>
+			<hr class="wp-header-end">
+			<hr/>
+		<?php
 	}
 }
