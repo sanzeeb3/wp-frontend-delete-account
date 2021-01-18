8acd6d16a7d648dd5769046f82b4b452a568b6f3
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2021-01-18 23:03:30 +0545

    Bring back the same option for backwards compat

--- src/Backend.php
+++ src/Backend.php
@@ -220,7 +220,7 @@ class Backend {
 			}
 
 			$email           = sanitize_text_field( $_GET['email'] );
-			$options = array( 'wpfda_title', 'wpfda_button_label', 'wpfda_attribute', 'wpfda_security', 'wpfda_security_password_text', 'wpfda_security_custom_captcha_question', 'wpfda_security_custom_captcha_answer', 'wpfda_load_assets_globally', 'wpfda_email_receipent', 'wpfda_enable_user_email', 'wpfda_enable_admin_email', 'wpfda_user_email_subject', 'wpfda_admin_email_subject', 'wp-frontend-delete-account-'. $email .'-email-editor' );
+			$options = array( 'wpfda_title', 'wpfda_button_label', 'wpfda_attribute', 'wpfda_security', 'wpfda_security_password_text', 'wpfda_security_custom_captcha_question', 'wpfda_security_custom_captcha_answer', 'wpfda_load_assets_globally', 'wpfda_email_receipent', 'wpfda_enable_user_email', 'wpfda_enable_admin_email', 'wpfda_user_email_subject', 'wpfda_admin_email_subject', 'wpfda_'. $email .'_email_message' );
 
 			foreach ( $options as $option ) {
 				if ( isset( $_POST[ $option ] ) ) {
@@ -229,9 +229,9 @@ class Backend {
 				}
 			}
 
-			if ( isset( $_POST ['wp-frontend-delete-account-'. $email .'-email-editor'] ) ) {
+			if ( isset( $_POST ['wpfda_' . $email . '_email_message'] ) ) {
 				$editor = wp_kses(
-					wp_unslash( $_POST['wp-frontend-delete-account-' . $email .'-email-editor'] ),
+					wp_unslash( $_POST['wpfda_' . $email . '_email_message'] ),
 					array(
 						'a'      => array(
 							'href'  => array(),
@@ -252,7 +252,7 @@ class Backend {
 					)
 				);
 
-				update_option( 'wp-frontend-delete-account-'. $email .'-email-editor', $editor );
+				update_option( 'wpfda_' . $email . '_email_message', $editor );
 			}
 		}
 	}
