37b3ef0884165dc8cb1edf297946ba0903b300c2
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2021-08-25 22:54:32 +0545

    A little bit of conditional rendering

--- assets/js/admin/review.js
+++ assets/js/admin/review.js
@@ -8,21 +8,37 @@
 
 import { render } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
 
 export default function Review() {
 
+	const [dismiss, setDismiss] = useState( 'off' );
+
+	const dismissReview = (e) => {
+		setDismiss( 'on' );
+	}
+
 	return (
-		<div id ='wp-frontend-delete-account-review-notice' className='notice notice-info wp-frontend-delete-account-review-notice'>
-			<div className ='wp-frontend-delete-account-review-thumbnail'>
-				<img src={wpfda_plugins_params.image_url} alt='' />
+
+		dismiss === 'off'
+
+		?
+			<div id ='wp-frontend-delete-account-review-notice' className='notice notice-info wp-frontend-delete-account-review-notice'>
+				<div className ='wp-frontend-delete-account-review-thumbnail'>
+					<img src={wpfda_plugins_params.image_url} alt='' />
+				</div>
+
+				<div className='wp-frontend-delete-account-review-text'>
+					<a onClick={dismissReview} id='wp-frontend-delete-account-dismiss' href='#'>{ __( 'Dismiss', 'wp-frontend-delete-account') }</a>
+					<h3> { __( 'Whoopee!', 'wp-frontend-delete-account' ) } </h3>
+					<p>How's it going? I hope that you found WP Frontend Delete Account helpful.<br/><br/>
+					Would you do me some favour and leave a  <a href="https://wordpress.org/support/plugin/wp-frontend-delete-account/reviews/?filter=5#new-post" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733; </a>review on <a href="https://wordpress.org/support/plugin/wp-frontend-delete-account/reviews/?filter=5#new-post" target="_blank"><strong>WordPress.org</strong></a> to help us spread the word and boost my motivation?</p>
+				</div>
 			</div>
 
-			<div className='wp-frontend-delete-account-review-text'>
-				<h3> { __( 'Whoopee!', 'wp-frontend-delete-account' ) } </h3>
-				<p>How's it going? I hope that you found WP Frontend Delete Account helpful.<br/><br/>
-				Would you do me some favour and leave a  <a href="https://wordpress.org/support/plugin/wp-frontend-delete-account/reviews/?filter=5#new-post" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733; </a>review on <a href="https://wordpress.org/support/plugin/wp-frontend-delete-account/reviews/?filter=5#new-post" target="_blank"><strong>WordPress.org</strong></a> to help us spread the word and boost my motivation?</p>
-			</div>
-		</div>
+		:
+
+		null
 	)
 }
 
